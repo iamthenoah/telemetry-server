8186a4baa68c711b7c3c546600af43d291b64ca6
noahgreff <greffnoah@gmail.com>
2023-10-27 13:53:13 +0200

    added excel and csv downloader

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const xlsx = require('xlsx')
+const json2csv = require('json2csv')
 const { getTables, getAll } = require('./database')
 
 const app = express()
@@ -8,7 +10,7 @@ const setupServer = () => {
 		res.sendStatus(200)
 	})
 
-	app.get('/tables', async (_, res) => {
+	app.get('/table', async (_, res) => {
 		try {
 			res.json(await getTables())
 		} catch (error) {
@@ -18,9 +20,28 @@ const setupServer = () => {
 
 	app.get('/table/:table', async (req, res) => {
 		try {
+			const file = req.query.file
+			const table = req.params.table
 			const page = req.query.page || 1
 			const count = req.query.count || 10
-			res.json(await getAll(req.params.table, page, count))
+			const data = await getAll(table, page, count)
+
+			if (file === 'csv' || file === 'xlsx') {
+				if (file === 'csv') {
+					const csv = json2csv.parse(data)
+					res.header('Content-Type', 'text/csv')
+					res.attachment(`${table}.csv`)
+					res.send(csv)
+				} else if (file === 'xlsx') {
+					const ws = xlsx.utils.json_to_sheet(data)
+					const wb = xlsx.utils.book_new()
+					xlsx.utils.book_append_sheet(wb, ws, table)
+					xlsx.writeFile(wb, `${table}.xlsx`)
+					res.download(`${table}.xlsx`)
+				}
+			} else {
+				res.json(data)
+			}
 		} catch (error) {
 			res.status(500).json({ error: error.message })
 		}
