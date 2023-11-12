cd494f8bbc9e95b0aeea6c7b3596407ddcb005c8
Noah <greffnoah@gmail.com>
2023-11-12 22:16:36 +0100

    removed unused route

--- src/server.js
+++ src/server.js
@@ -1,29 +1,18 @@
-const { getTables, insertOne } = require('./database')
 const express = require('express')
 const multer = require('multer')
-const uuid = require('uuid')
-
-const app = express()
-app.use(multer().array())
 
 let player = null
 
 const getCurrentPlayer = () => player
 
+const app = express()
+app.use(multer().array())
+
 const setupServer = () => {
 	app.use(express.static('public'))
 
-	app.get('/table', async (_, res) => {
-		try {
-			res.json(await getTables())
-		} catch (error) {
-			res.status(500).json({ error: error.message })
-		}
-	})
-
 	app.post('/player', async (req, res) => {
-		player = { playerId: uuid.v4(), ...Object.fromEntries(Object.entries(req.body)) }
-		await insertOne('player', player)
+		player = Object.fromEntries(Object.entries(req.body))
 		res.sendStatus(201)
 	})
 
